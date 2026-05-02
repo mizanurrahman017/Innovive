ec7132d7c93ffb7b234cee5c2ad15b5cf9d29e9f
unknown <mizanasif1@gmail.com>
2026-05-02 23:11:16 +0600

    cnt korlam

--- src/Pages/Root/Home/Home.jsx
+++ src/Pages/Root/Home/Home.jsx
@@ -4,11 +4,7 @@ import NewArrival from '../../../Components/NewArrival/NewArrival';
 import Airpods from '../../../Components/Airpods/Airpods';
 import Clock from '../../../Components/Clock/Clock';
 import Speaker from '../../../Components/Speaker/Speaker';
-// ami
-// ami holam
-// ami holam developer
-// ami holam developer and frontend 
-// ami holam developer and frontend developer
+// ami holam developer and frontend and
 
 
 const Home = () => {
