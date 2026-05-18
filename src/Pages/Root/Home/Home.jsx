42fc312e69ad2761c902db48305f09fe4bd8ca47
unknown <mizanasif1@gmail.com>
2026-05-18 21:30:05 +0600

    new kori cmnt

--- src/Pages/Root/Home/Home.jsx
+++ src/Pages/Root/Home/Home.jsx
@@ -6,6 +6,7 @@ import Clock from '../../../Components/Clock/Clock';
 import Speaker from '../../../Components/Speaker/Speaker';
 
 // ami holam web dev
+// hello ami web developer
 const Home = () => {
     return (
         <div >
